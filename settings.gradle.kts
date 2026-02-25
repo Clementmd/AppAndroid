6b0944e6d30067c140d2609732645392acfcee68
Clement Michaud <michaudc@hostname.iut2.upmf-grenoble.fr>
2026-02-25 10:25:33 +0100

    ajout du repertoire

---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ecole_des_loustiques_Michaud_Clement"
+include(":app")
+ 
