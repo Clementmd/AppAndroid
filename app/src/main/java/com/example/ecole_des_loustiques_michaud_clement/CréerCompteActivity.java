a4fc22cedd3ba795f174a48053bccf8fb548ff8e
Clement Michaud <michaudc@hostname.iut2.upmf-grenoble.fr>
2026-03-06 15:12:16 +0100

    fin de tous les fichiers xml

--- app/src/main/java/com/example/ecole_des_loustiques_michaud_clement/CréerCompteActivity.java
+++ app/src/main/java/com/example/ecole_des_loustiques_michaud_clement/CréerCompteActivity.java
@@ -14,7 +14,7 @@ public class CréerCompteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_accueil);
+        setContentView(R.layout.activity_creercompte);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
